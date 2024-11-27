ec8ce78b3560fdc845f3b3dae87077df68f584ba
Zxuuuu <1521786784@qq.com>
2024-11-27 13:12:20 +0800

    update

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 import nailong_UI
 import nailong_utils as utils
-import sys
-import os
-
-sys.path.append('D:\Projects\\torch\\nailong')
+# import sys
+# import os
+#
+# sys.path.append('D:\Projects\\torch\\nailong')
 
 if __name__ == "__main__":
     win = nailong_UI.create_main_window()
